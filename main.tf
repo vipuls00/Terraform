8f2b16be1a46c350fe86b2cac141df5f78f67626
Vipul Sharma <87320828+vipuls00@users.noreply.github.com>
2024-03-18 09:59:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
   features {}
+  subscription_id = "538cddba-8674-4d0d-a1ce-485a41d64c22"
+  client_id       = "464385df-ee45-4c74-815d-c0371a2e7ac3"
+  client_secret   = "~sQ8Q~s-WahnxQsTQUMDp2hK5w2fniXJK3cE0dnJ"
+  tenant_id       = "b62f3499-9c4d-4409-96a7-c104c6a512a9"
 }
 
 resource "azurerm_storage_account" "TfStorageAccount" {
